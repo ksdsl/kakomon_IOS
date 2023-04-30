446b6e499121033b329c55cc9af388f00b899bb7
ks75167 <ksdnhy@gmail.com>
2023-04-30 13:20:46 +0900

    2023-04-30

--- MyApp1/src/screens/ViewPNGScreen.js
+++ MyApp1/src/screens/ViewPNGScreen.js
@@ -1,21 +1,52 @@
+//import React, { Component } from 'react';
+//import { View, Image } from 'react-native';
+
+// export default class App extends Component {
+//   render() {
+//     return (
+//       <View>
+//         <Image
+//           style={{
+//             width: 64,
+//             height: 64
+//           }}
+//           source={require('../PNG/test.png')}
+      
+        
+//         />
+//       </View>
+//     );
+//   }
+// }
 import React, { Component } from 'react';
-import { View, Image } from 'react-native';
+import {
+  AppRegistry,
+  StyleSheet,
+  View,
+} from 'react-native';
+import { SketchCanvas } from '@terrylinla/react-native-sketch-canvas';
+
 
 export default class App extends Component {
   render() {
     return (
-      <View>
-        <Image
-          style={{
-            width: 64,
-            height: 64
-          }}
-          //source={{uri: 'https://reactnative.dev/img/tiny_logo.png'}}
-          source={require('../PDF/test.png')}
-      
-        
-        />
+      <View style={styles.container}>
+        <View style={{ flex: 1, flexDirection: 'row' }}>
+          <SketchCanvas
+            style={{ flex: 1 }}
+            strokeColor={'red'}
+            strokeWidth={7}
+          />
+        </View>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#F5FCFF',
+  },
+});
+
+AppRegistry.registerComponent('App', () => App);
